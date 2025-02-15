6068f3d1f539c49d1dd7b43f710e7df7af14c2c3
Ángela Donoso <angeladonoso12@gmail.com>
2025-02-15 14:56:48 +0100

    Diseño del formulario y la cabecera
    Cambios sobre el componente del formulario para establecer el diseño y la funcionalidad de restar caracteres.

--- miBlog/src/app/app.component.ts
+++ miBlog/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormularioComponent, PostsComponent, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'miBlog';
-}
+export class AppComponent {}
--- miBlog/src/app/components/formulario/formulario.component.ts
+++ miBlog/src/app/components/formulario/formulario.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulario.component.html',
-  styleUrl: './formulario.component.css'
+  styleUrl: './formulario.component.css',
 })
 export class FormularioComponent {
+  resena: string = '';
+  maxLength: number = 500;
+  caracteresRestantes: number = this.maxLength;
 
+  onInput() {
+    this.caracteresRestantes = this.maxLength - this.resena.length;
+  }
 }
