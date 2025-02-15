034bf536d234cd160b15c8b5bae060d75ab287df
Ángela Donoso <angeladonoso12@gmail.com>
2025-02-15 16:28:26 +0100

    Input del Array al HTML
    Relaciono el array de libros con el componente de la lista de los libros

--- miBlog/src/app/components/posts/posts.component.ts
+++ miBlog/src/app/components/posts/posts.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IBooks } from '../../interfaces/ibooks.interface';
 
 @Component({
   selector: 'app-posts',
   imports: [],
   templateUrl: './posts.component.html',
-  styleUrl: './posts.component.css'
+  styleUrl: './posts.component.css',
 })
 export class PostsComponent {
+  @Input() booklist: IBooks[] = [];
+  text: string = '';
 
+  ngOnInit() {
+    this.booklist.forEach((book) => {
+      this.text += `<p> El título del libro es ${book.title}`; // En el template literal meteríamos el HTML para crear cada uno de los divs con su libro maquetado.
+    });
+  }
 }
