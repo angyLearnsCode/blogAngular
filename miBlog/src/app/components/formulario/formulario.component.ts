93a9f5b5471d4b45db73becb3958ff20c3a3068f
Ángela Donoso <angeladonoso12@gmail.com>
2025-02-16 14:07:10 +0100

    El botón hace correctamente el consolelog
    Se aplica funcionalidad al botón y representa los datos correctamente.

--- miBlog/src/app/components/formulario/formulario.component.ts
+++ miBlog/src/app/components/formulario/formulario.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IBooks } from '../../interfaces/ibooks.interface';
 
 @Component({
   selector: 'app-formulario',
@@ -8,11 +9,22 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './formulario.component.css',
 })
 export class FormularioComponent {
-  resena: string = '';
   maxLength: number = 500;
   caracteresRestantes: number = this.maxLength;
+  model = { resena: '' };
 
+  newBook: IBooks = {
+    title: '',
+    date: '',
+    urlPhoto: '',
+    text: '',
+  };
   onInput() {
-    this.caracteresRestantes = this.maxLength - this.resena.length;
+    this.newBook.text = this.model.resena;
+    this.caracteresRestantes = this.maxLength - this.model.resena.length;
+  }
+
+  getBook() {
+    console.log(this.newBook);
   }
 }
