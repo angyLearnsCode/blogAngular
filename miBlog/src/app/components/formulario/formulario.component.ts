9e47e83ea9a43392bfad56d13ba068a9fb27889b
Ángela Donoso <angeladonoso12@gmail.com>
2025-02-17 13:19:26 +0100

    Arreglo el error de la validación de URL
    Ahora solo salta el error al pinchar fuera del input si este detecta que la URL no es válida

--- miBlog/src/app/components/formulario/formulario.component.ts
+++ miBlog/src/app/components/formulario/formulario.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IBooks } from '../../interfaces/ibooks.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-formulario',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css',
 })
@@ -12,6 +13,7 @@ export class FormularioComponent {
   maxLength: number = 500;
   caracteresRestantes: number = this.maxLength;
   model = { resena: '' };
+  urlInvalid: boolean = false;
 
   newBook: IBooks = {
     title: '',
@@ -34,5 +36,23 @@ export class FormularioComponent {
       urlPhoto: '',
       text: '',
     };
+  }
+
+  isFormValid() {
+    return (
+      this.newBook.title &&
+      this.newBook.date &&
+      this.newBook.urlPhoto &&
+      this.model.resena
+    );
+  }
+
+  isUrlValid(url: string): boolean {
+    const regex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+    return regex.test(url);
+  }
+
+  onUrlBlur() {
+    this.urlInvalid = !this.isUrlValid(this.newBook.urlPhoto);
   }
 }
